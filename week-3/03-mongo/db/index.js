6dfc8fb8eab5a804d79b906e67a766c1da88fa82
Anubhav <anubhavsamanta023@gmail.com>
2024-01-12 09:20:06 +0530

    Revert "solved 03/mongo"
    This reverts commit 5797c7d38865e6dba08562d7f14aa49f078d38cb.

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,36 +1,24 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('connect mongodb');
+mongoose.connect('mongodb+srv://anubhavmdb:5GSiTXFUqpVyNNco@cluster0.49h1psc.mongodb.net/course-app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    username: String,
-    password: String,
+    // Schema definition here
 });
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    purchasedCourses: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course"
-    }]
+    // Schema definition here
 });
 
 const CourseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
-    imgLink: String,
-    published: Boolean,
+    // Schema definition here
 });
 
-//compiling the schemas
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
-
 
 module.exports = {
     Admin,
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,75 +1,22 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    try {
-        await User.create({
-            username: username,
-            password: password,
-        });
-
-        res.json({
-            msg: "User created successfully."
-        })
-
-    } catch (err) {
-        res.json({
-            msg: `${err.message}`
-        })
-    }
+router.post('/signup', (req, res) => {
+    // Implement user signup logic
 });
 
-router.get('/courses', async (req, res) => {
-    try {
-        const response = await Course.find();
-        res.json({
-            msg: response
-        })
-    } catch (err) {
-        res.json({
-            msg: `${err.message}`
-        })
-    }
+router.get('/courses', (req, res) => {
+    // Implement listing all courses logic
 });
 
-router.post('/courses/:courseId', userMiddleware, async (req, res) => {
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const courseId = req.params.courseId;
-    try {
-        await User.updateOne(
-            {
-                username: username,
-                password: password,
-            },
-            { $push: { purchasedCourses: courseId } }
-        )
-        res.json({
-            msg: `Purchased course successfully.`
-        })
-    } catch (err) {
-        res.json({
-            msg: `${err.message}`
-        })
-    }
+router.post('/courses/:courseId', userMiddleware, (req, res) => {
+    // Implement course purchase logic
 });
 
-router.get('/purchasedCourses', userMiddleware, async (req, res) => {
-    const username = req.headers.username;
-    const password = req.headers.password;
-    try {
-        const response = await User.findOne({ username: username, password: password }).populate('purchasedCourses', 'title');
-        res.json({
-            msg: response
-        })
-    } catch (err) {
-        console.error(err);
-    }
+router.get('/purchasedCourses', userMiddleware, (req, res) => {
+    // Implement fetching purchased courses logic
 });
 
 module.exports = router
